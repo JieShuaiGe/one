7f671339580d0cc8288e2ce84804eae32a917410
XiangWenJie <1304056844@qq.com>
2018-03-24 16:01:45 +0800

    fff

---
+++ MyTest.java
@@ -0,0 +1,20 @@
+package cn.com.webxml.test;
+
+import cn.com.webxml.MobileCodeWS;
+import cn.com.webxml.MobileCodeWSSoap;
+
+/**  
+ * ClassName:MyTest <br/>  
+ * Function:  <br/>  
+ * Date:     2018年3月18日 下午3:49:58 <br/>       
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        MobileCodeWS mobileCodeWS =new MobileCodeWS();
+        MobileCodeWSSoap mobileCodeWSSoap = mobileCodeWS.getMobileCodeWSSoap();
+        String bbs = mobileCodeWSSoap.getMobileCodeInfo("18907734", "");
+		
+        System.out.println(bbs);
+    }
+}
+  
